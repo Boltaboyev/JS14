291771df6b596d097e3efc49a528b0b9f517676c
Otabekcdr <boltaboyevotabek207@gmail.com>
2024-12-13 11:04:16 +0500

    j

---
+++ app.js
@@ -0,0 +1,29 @@
+// 1 ---------------------------------------------------------
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     get info() {
+//         console.log(`${this.name}'s age is ${this.age}`);
+//     }
+// }
+// const person = new Person('John', 34);
+// person.info;
+
+// 3 -------------------------------------------------------------
+// class Animal {
+//     constructor(nick, type) {
+//         this.name = nick
+//         this.type = type
+//     }
+//     get Name() {
+//         return this.name
+//     }
+//     str() {
+//         console.log(`${this.Name} is a ${this.type}`)
+//     }
+// }
+
+// let animal = new Animal("Max", "dog")
+// animal.str()
